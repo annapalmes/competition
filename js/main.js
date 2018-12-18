e3616474c9ee4a4946e4ffa4cb9c30cb1034564f
anna palmes <anna_palmes@hotmail.com>
2018-12-18 10:21:41 +0100

    commit2

---
+++ js/main.js
@@ -0,0 +1,282 @@
+var myMusic = object.music;
+console.log(myMusic);
+
+var myTeams = object.teams;
+console.log(myTeams);
+
+getdate();
+listener();
+hideShow("home");
+
+// date //
+
+function getdate() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+
+    function checkTime(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
+    m = checkTime(m);
+
+    var month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var result = `${weekday[date.getDay()]} ${date.getDate()} ${month[date.getMonth()]} ${h}:${m}h.`;
+
+    document.getElementById("date").append(result);
+
+}
+
+// hide and show //
+
+function hideShow(page) {
+
+    var pages = document.getElementsByClassName("container");
+
+    for (var n = 0; n < pages.length; n++) {
+        pages[n].style.display = "none";
+
+    }
+
+    switch (page) {
+        case "music":
+            music();
+            break;
+        case "chat":
+            getPosts();
+            break;
+        case "ranking":
+            teams();
+            break;
+        case "teams":
+            showmembers(event.target.textContent);
+            break;
+    }
+
+    document.getElementById(page).style.display = "";
+}
+
+function listener() {
+    var links = document.querySelectorAll("[data-page]"); //seleccionar todos los data-page y guardarlos en array                                                           links
+    for (var i = 0; i < links.length; i++) { //loop en array links, y poner eventListener click
+        links[i].addEventListener("click", function () {
+            var page = this.getAttribute("data-page"); //coger valor del attr data-page
+            hideShow(page); //enseñar div con id igual al valor del data-page    
+        })
+    }
+}
+ 
+// chat //
+
+document.getElementById("login").addEventListener("click", login);
+
+function login() {
+
+    // https://firebase.google.com/docs/auth/web/google-signin
+
+    // Provider
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+        
+    // How to Log In
+
+    firebase.auth().signInWithPopup(provider).then(function(){ 
+        myProfile();
+    }).then;
+
+    console.log("login");
+
+}
+
+function writeNewPost() {
+
+    // https://firebase.google.com/docs/database/web/read-and-write
+
+    var textToSend = document.getElementById("textInput").value;
+
+    // Values
+
+    var message = {
+        message: textToSend,
+        name: firebase.auth().currentUser.displayName
+    }
+
+
+    firebase.database().ref('chat').push(message)
+
+    console.log(message);
+
+    // A post entry.
+
+    // Get a key for a new Post.
+
+    // Write data
+
+    console.log("write");
+}
+
+function getPosts() {
+    var posts = document.getElementById("posts");
+    firebase.database().ref('chat').on('value', function (data) {
+        
+        posts.innerHTML = "";
+
+        var messages = data.val();
+
+        for (var key in messages) {
+            var text = document.createElement("div");
+            var element = messages[key];
+             
+            text.setAttribute("class", "message");
+            
+            text.append(element.name + (":") + (" "));
+            text.append(element.message);
+            posts.append(text);
+        }
+        posts.scrollTop = posts.scrollHeight;
+
+    })
+
+    console.log("getting posts");
+
+}
+
+// music //
+
+document.getElementById("music").addEventListener("click", music);
+
+function music() {
+ 
+    var songs = document.getElementById("songs");
+    
+   songs.innerHTML = "";
+    
+    for (var n = 0; n < object.music.length; n++) {
+
+        var names = document.createElement("p");
+        var song = document.createElement("span");
+        var singer = document.createElement("span");
+
+        song.setAttribute("class", "song");
+        singer.setAttribute("class", "singer");
+
+        var audio = document.createElement("audio")
+        audio.setAttribute("controls", null)
+        var source = document.createElement("source")
+        source.setAttribute("src", object.music[n].url)
+        source.setAttribute("type", "audio/mpeg")
+        audio.append(source)
+
+        song.append(object.music[n].song_name);
+        singer.append(object.music[n].singer_name);
+        names.append(song, singer);
+        source.append(object.music[n].url);
+
+        songs.append(names, audio);
+
+    }
+
+}
+
+// members //
+
+function showmembers(teamname) {
+    console.log(teamname);
+    
+    var teaminfo= myTeams.find(cadateam => cadateam.name == teamname); 
+
+    var tbody = document.getElementById("infomember");
+    tbody.innerHTML = "";
+    for (var n = 0; n < teaminfo.members.length; n++) {
+
+        var row = document.createElement("tr");
+
+        var cellname = document.createElement("td");
+        var cellage = document.createElement("td");
+        var cellnacionality = document.createElement("td");
+
+        var names = teaminfo.members[n].name;
+        var ages =teaminfo.members[n].age;
+        var nacionalities = teaminfo.members[n].nacionality;
+
+        cellname.append(names);
+        cellage.append(ages);
+        cellnacionality.append(nacionalities);
+    
+        row.append(cellname);
+        row.append(cellage);
+        row.append(cellnacionality);
+
+        tbody.append(row);
+
+    }
+    
+}
+
+// teams // 
+
+function teams() {
+
+    var team = document.getElementById("listteams");
+    team.innerHTML = "";
+
+    for (var n = 0; n < myTeams.length; n++) {
+
+        var name = document.createElement("li");
+
+        name.setAttribute("data-page", "teams");
+        name.setAttribute("data-team", myTeams[n].name);
+
+        name.append(myTeams[n].name);
+        team.append(name);
+    }
+    listener();
+}
+
+// profile //
+
+function myProfile(){
+
+var profile = document.getElementById("profile");
+
+
+        var photo = document.createElement("img");
+        var name = document.createElement("p");
+        var mail = document.createElement("p");
+        
+        photo.setAttribute("src", firebase.auth().currentUser.photoURL);
+        photo.setAttribute("class", "profile2");
+        name.setAttribute("class", "nameandmail");
+        mail.setAttribute("class", "nameandmail");
+        
+        name.append(firebase.auth().currentUser.displayName);
+        mail.append(firebase.auth().currentUser.email);
+        
+        profile.append(photo, name, mail);
+
+}
---
+++ json.js
@@ -0,0 +1,352 @@
+var object = {
+
+    "music": [{
+
+            "song_name": "beggin",
+            "singer_name": "madcon",
+            "url": "songs/Beggin'.mp3"
+              },
+        {
+            "song_name": "move if you wanna",
+            "singer_name": "mims",
+            "url": "songs/MIMS%20-%20Move%20(If%20You%20Wanna).mp3",
+        },
+        {
+            "song_name": "low",
+            "singer_name": "florida",
+            "url": "songs/Flo%20Rida%20ft.%20T-Pain%20-%20Low%20%20LYRICS.mp3",
+              },
+        {
+            "song_name": "u don't like me",
+            "singer_name": "diplo",
+            "url": "songs/Step%20up%20Revolution%20Diplo%20feat.%20Lil%20Jon%20-%20U%20Dont%20Like%20Me%20(Datsik%20Remix).mp3",
+              },
+        {
+            "song_name": "bring it back",
+            "singer_name": "travis forter",
+            "url": "songs/Travis%20Porter%20-%20Bring%20It%20Back.mp3",
+              },
+        {
+            "song_name": "the way i are",
+            "singer_name": "timbaland",
+            "url": "songs/Timbaland%20-%20The%20Way%20I%20Are%20ft.%20Keri%20Hilson,%20D.O.E.,%20Sebastian.mp3",
+              }],
+
+    "teams": [{
+
+            "name": "jabbawockezz",
+            "years_active": " 2003 - present",
+            "origin": " San Diego, California",
+            "website": "www.jbwkz.com",
+            "members": [{
+
+                    "name": "Kevin Kb Brewer",
+                    "age": "22",
+                    "nacionality": "los angeles, california",
+        
+             },
+                {
+                    "name": "Joe Punkee Larot",
+                    "age": "27",
+                    "nacionality": "San Diego, california ",
+
+                     },
+                {
+                    "name": "Jeff Phi Nguyen",
+                    "age": "20",
+                    "nacionality": "sacramento, california",
+
+                     },
+                {
+                    "name": "Phil SB Tayag",
+                    "age": "24",
+                    "nacionality": "San Diego, california",
+
+                     },
+                {
+                    "name": "Rainen KidRainen Paguio",
+                    "age": "22",
+                    "nacionality": "san francisco, california",
+
+                     },
+
+                     ]
+    },
+        {
+
+
+            "name": "the wild",
+            "years_active": " 2010 - present",
+            "origin": " Madrid, Spain",
+            "website": " ",
+            "members": [{
+
+                    "name": "María Díaz",
+                    "age": "24",
+                    "nacionality": "Madrid, Spain",
+             },
+                {
+                    "name": "Carla Álvarez",
+                    "age": "18",
+                    "nacionality": "Barcelona, Spain",
+                     },
+                {
+                    "name": "Juan Torres ",
+                    "age": "26",
+                    "nacionality": "Madrid, Spain",
+                     },
+                {
+                    "name": "Miguel Ruiz",
+                    "age": "20",
+                    "nacionality": "Sevilla, Spain",
+                     },
+                {
+                    "name": "Isabel Giménez",
+                    "age": "21",
+                    "nacionality": "Madrid, Spain",
+                     },
+
+                     ]
+    },
+        {
+
+            "name": "diversity",
+            "years_active": "2007 - prsent",
+            "origin": "London, United Kingdom",
+            "website": "www.diversitystreetdance.net",
+            "members": [{
+
+                    "name": "Ashley Banjo",
+                    "age": "30",
+                    "nacionality": "Leytonstone, London, England",
+
+             },
+                {
+                    "name": "Jordan Banjo",
+                    "age": "25",
+                    "nacionality": "United Kingdom",
+
+                     },
+                {
+                    "name": "Mitchell Craske",
+                    "age": "27",
+                    "nacionality": " bristol , england",
+
+                     },
+                {
+                    "name": "Sam Craske",
+                    "age": "25",
+                    "nacionality": " london , england",
+
+                     },
+                {
+                    "name": "Perri Kiely",
+                    "age": "22",
+                    "nacionality": "United Kingdom",
+
+                     },
+
+
+                     ]
+    },
+        {
+
+            "name": "request dance crew",
+            "years_active": "2007-prsent",
+            "origin": "Auckland New Zealand",
+            "website": "thepalacedancestudio.co.nz",
+            "members": [{
+
+                    "name": "Paris Goebel",
+                    "age": "27",
+                    "nacionality": "Manurewa, Auckland ,New Zealand",
+
+
+             },
+                {
+                    "name": "Álex Mendes",
+                    "age": "23",
+                    "nacionality": "Auckland New Zealand",
+
+                     },
+                {
+                    "name": "Althe Strydom",
+                    "age": "22",
+                    "nacionality": "Auckland New Zealand",
+
+                     },
+                {
+                    "name": "Kaea Pearce",
+                    "age": "30",
+                    "nacionality": "Auckland New Zealand",
+
+                     },
+                {
+                    "name": "Kristen Dodgen",
+                    "age": "19",
+                    "nacionality": "Auckland New Zealand",
+
+                     },
+
+                     ]
+    },
+        {
+
+            "name": "poreotics",
+            "years_active": "2007-prsent",
+            "origin": "Westminster,California",
+            "website": " ",
+            "members": [{
+
+                    "name": "Matt Dumbo Nguyen",
+                    "age": "31",
+                    "nacionality": "Westminster,California",
+
+
+             },
+                {
+                    "name": "Can Trong Nguyen",
+                    "age": "26",
+                    "nacionality": "Westminster,California",
+
+                     },
+                {
+                    "name": "Lawrence Bravo",
+                    "age": "28",
+                    "nacionality": "Westminster,California",
+
+                     },
+                {
+                    "name": "Andrew Fausto",
+                    "age": "28",
+                    "nacionality": "Westminster,California",
+
+                     },
+                {
+                    "name": " Aidan Carberry",
+                    "age": "25",
+                    "nacionality": "Westminster,California",
+
+                     },
+
+                     ]
+    },
+        {
+
+            "name": "sol-t-shine",
+            "years_active": "2009-present",
+            "origin": "Osaka, Japan ",
+            "website": " ",
+            "members": [{
+
+                    "name": "Kirara Matsuda",
+                    "age": "18",
+                    "nacionality": "Osaka, Japan",
+
+             },
+                {
+                    "name": "Kako Tokumoto",
+                    "age": "21",
+                    "nacionality": " Tokyo , Japan",
+
+                     },
+                {
+                    "name": "Nanase Tomimura",
+                    "age": "26",
+                    "nacionality": "Osaka, Japan",
+
+                     },
+                {
+                    "name": "Hinano Nishihara",
+                    "age": "18",
+                    "nacionality": " Hakone, Japan",
+
+                     },
+                {
+                    "name": "Hijiri Matsudo",
+                    "age": "22",
+                    "nacionality": "Kyoto, Japan",
+
+                     },
+
+                     ]
+    },
+        {
+
+            "name": "philippine all stars",
+            "years_active": "2007-present",
+            "origin": "Manila, Philippines",
+            "website": " ",
+            "members": [{
+
+                    "name": "Kenjhons Serrano",
+                    "age": "22",
+                    "nacionality": "Manila, Philippines",
+
+
+             },
+                {
+                    "name": "Chelo Aestrid",
+                    "age": "22",
+                    "nacionality": "Manila, Philippines",
+
+                     },
+                {
+                    "name": "Maya Carandang",
+                    "age": "23",
+                    "nacionality": "Manila, Philippines",
+
+                     },
+                {
+                    "name": "Michelle Tzy Salazar",
+                    "age": "25",
+                    "nacionality": "Manila, Philippines",
+
+                     },
+                {
+                    "name": "Jhong Mesina",
+                    "age": "20",
+                    "nacionality": "Manila, Philippines",
+
+                     },
+
+                     ]
+    },
+        {
+
+            "name": "the royal family",
+            "years_active": "2010-present",
+            "origin": "Auckland, Nueva Zelanda",
+            "website": " ",
+            "members": [{
+
+                    "name": "Ashleigh MacKinven",
+                    "age": " 18",
+                    "nacionality": "Auckland, Nueva Zelanda",
+             },
+                {
+                    "name": "Shalom Leilua",
+                    "age": "19",
+                    "nacionality": "Auckland, Nueva Zelanda",
+                     },
+                {
+                    "name": "Bonnie Talamaivao",
+                    "age": "18",
+                    "nacionality": "Auckland, Nueva Zelanda",
+                     },
+                {
+                    "name": "Courtney Hale",
+                    "age": "20",
+                    "nacionality": "Auckland, Nueva Zelanda",
+                     },
+                {
+                    "name": "Kaili Bright",
+                    "age": "21",
+                    "nacionality": "Auckland, Nueva Zelanda",
+                     },
+
+                     ]
+    }],
+    
+
+}
